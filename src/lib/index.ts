ac7aaecc64a12bc10041ae3605aa3134bf03f26e
Louis Li <lee178lee@gmail.com>
2024-09-21 15:31:16 -0700

    Add set_pdfjs_context export to index.ts

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,3 +1,4 @@
 export { default as Document } from './PDFViewer/Document.svelte';
 export { default as Page } from './PDFViewer/Page.svelte';
+export { set_pdfjs_context } from './utils/vite'
 export * from './utils/target_dimension.js';
