3e027860e6c5747ea12df9edda32fb8d86488520
Louis Li <lee178lee@gmail.com>
2024-09-21 00:39:59 -0700

    update text layer to use new api

--- src/lib/PDFViewer/PageInternals/TextLayer.svelte
+++ src/lib/PDFViewer/PageInternals/TextLayer.svelte
@@ -1,23 +1,25 @@
 <script lang="ts">
 	import { BROWSER } from 'esm-env';
-	import type { PageViewport, PDFPageProxy, TextLayerRenderTask } from 'pdfjs-dist';
+	import type { PageViewport, PDFPageProxy, TextLayer } from 'pdfjs-dist/legacy/build/pdf.mjs';
 
 	export let page: PDFPageProxy;
 	export let viewport: PageViewport;
 
-	let render_task: TextLayerRenderTask;
+	let render_task: TextLayer;
 	let container: HTMLDivElement;
 
+	
 	async function render_text_layer() {
 		container.textContent = '';
-		const { renderTextLayer } = await import('pdfjs-dist');
+		const {TextLayer} = await import('pdfjs-dist/legacy/build/pdf.mjs');
 
 		render_task?.cancel();
-		render_task = renderTextLayer({
+		render_task = new TextLayer({
 			container,
 			textContentSource: page.streamTextContent(),
 			viewport,
 		});
+		await render_task.render();
 	}
 
 	$: if (BROWSER && container && viewport) render_text_layer();
